44dbd24d2835a298212c9beacc825eb0fba658b8
ardithyseni <ah49393@ubt-uni.net>
2022-05-13 20:26:51 +0200

    finished pagination, sorting, searching in the API

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,9 +1,11 @@
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json;
 using System.Threading.Tasks;
 using API.Data;
 using API.Entities;
 using API.Extensions;
+using API.RequestHelpers;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -21,17 +23,22 @@ namespace API.Controllers
         }
 
         [HttpGet]
-        public async Task<ActionResult<List<Product>>> GetProducts(string orderBy, 
-            string searchTerm, string brands, string types)
+        public async Task<ActionResult<PagedList<Product>>> GetProducts([FromQuery]ProductParams productParams)
         {
             var query = _context.Products
-                .Sort(orderBy)
-                .Search(searchTerm)
-                .Filter(brands, types)
+                .Sort(productParams.OrderBy)
+                .Search(productParams.SearchTerm)
+                .Filter(productParams.Brands, productParams.Types)
                 .AsQueryable();
 
-            return await query.ToListAsync();
+            // return await query.ToListAsync();
+            var products = await PagedList<Product>.ToPagedList(query, 
+                productParams.PageNumber, productParams.PageSize);
 
+            Response.AddPaginationHeader(products.MetaData); // metoda te Http Extensions
+            
+
+            return products;
         }
 
         [HttpGet("{id}")] // api/products/3
@@ -42,6 +49,16 @@ namespace API.Controllers
             if (product == null) return NotFound();
             
             return product;
+        }
+
+
+        [HttpGet("filters")]
+        public async Task<IActionResult> GetFilters()
+        {
+            var brands = await _context.Products.Select(p => p.Brand).Distinct().ToListAsync();
+            var types = await _context.Products.Select(p => p.Type).Distinct().ToListAsync();
+        
+            return Ok(new {brands, types});
         }
 
     }
---
+++ API/RequestHelpers/PagedList.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.RequestHelpers
+{
+    public class PagedList<T> : List<T>
+    {
+        public PagedList(List<T> items, int count, int pageNumber, int pageSize)
+        {
+            MetaData = new MetaData
+            {
+                TotalCount = count,
+                PageSize = pageSize,
+                CurrentPage = pageNumber,
+                TotalPages = (int)Math.Ceiling(count / (double)pageSize)
+            };
+            AddRange(items);
+        }
+
+        public MetaData MetaData { get; set; }
+
+        public static async Task<PagedList<T>> ToPagedList(IQueryable<T> query, 
+            int pageNumber, int pageSize)
+        {
+            var count = await query.CountAsync();
+            // if pageSize is 10, we want the second page of 18 elements, skip (2 - 1 * 10). take the next remaining
+            var items = await query.Skip((pageNumber - 1) * pageSize).Take(pageSize).ToListAsync();
+
+            return new PagedList<T>(items, count, pageNumber, pageSize);
+        }
+    }
+}
