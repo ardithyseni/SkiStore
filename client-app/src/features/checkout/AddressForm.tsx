df9bbd78cd622892cf523d356b03733c3bb65c74
ardithyseni <ah49393@ubt-uni.net>
2022-07-29 13:28:58 +0200

    checkout steps initialized

---
+++ client-app/src/features/checkout/AddressForm.tsx
@@ -0,0 +1,71 @@
+import * as React from 'react';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import { useForm } from 'react-hook-form';
+import AppTextInput from '../../app/components/AppTextInput';
+import { Button } from '@mui/material';
+
+export default function AddressForm() {
+    const { control, handleSubmit } = useForm();
+    return (
+        <>
+            <Typography variant="h6" gutterBottom>
+                Shipping address
+            </Typography>
+            <form action="" onSubmit={handleSubmit((data) => console.log(data))}>
+            <Grid container spacing={3}>
+                <Grid item xs={12} sm={12}>
+                    <AppTextInput
+                        control={control}
+                        name='fullName'
+                        label='Full Name'
+                    />
+                </Grid>
+
+                <Grid item xs={12}>
+                    <AppTextInput
+                        control={control}
+                        name='address1'
+                        label='Address 1'
+                    />
+                </Grid>
+
+                <Grid item xs={12} sm={6}>
+                    <AppTextInput
+                        control={control}
+                        name='city'
+                        label='City'
+                    />
+                </Grid>
+
+                <Grid item xs={12} sm={6}>
+                    <AppTextInput
+                        control={control}
+                        name='zip'
+                        label='Zip Code'
+                    />
+                </Grid>
+
+                <Grid item xs={12} sm={6}>
+                    <AppTextInput
+                        control={control}
+                        name='country'
+                        label='Country'
+                    />
+                </Grid>
+
+                <Grid item xs={12}>
+                    <FormControlLabel
+                        control={<Checkbox color="secondary" name="saveAddress" value="yes" />}
+                        label="Use this address for payment details"
+                    />
+                </Grid>
+            </Grid>
+            <Button type='submit'>Submit Form</Button>
+            </form>
+        </>
+    );
+}
